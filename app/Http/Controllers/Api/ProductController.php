abe4f78b96f96aeead92fee8fa75fb5c17f76c77
Mateo Rada <mateora73@gmail.com>
2025-06-25 20:09:34 -0500

    Created basic CRUD operations

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Product;
+use Illuminate\Support\Facades\Validator;
+
+class ProductController extends Controller
+{
+    public function getAll()
+    {
+        return Product::all();
+    }
+
+    public function getByID($id)
+    {   
+        $product = Product::find($id);
+
+        if(!$product){
+            return response()->json(
+                [
+                    'message' => 'No product with that ID was found'
+                ], 200
+            );
+        }
+        return response()->json([$product],200);
+    }
+
+    public function save(Request $req)
+    {
+        $valid = Validator::make($req->all(), [
+            'name' => 'required',
+            'description' => 'required',
+            'code' => 'required|unique:product',
+            'status' => 'required'
+        ]);
+
+        if ($valid->fails()) {
+            return response()->json(
+                [
+                    'message' => 'Some fields are missing or are incorrect, please check the request',
+                    'errors' => $valid->errors()
+                ], 200);
+        }
+
+        $product = Product::create([
+            'name' => $req->name,
+            'description' => $req->description,
+            'code' => $req->code,
+            'status' => $req->status
+        ]);
+
+        if (!$product) {
+            return response()->json(['message' => 'Error creating the product'], 500);
+        }
+
+        return response()->json($product, 200);
+    }
+
+    public function updateByID($id,Request $req)
+    {   
+
+        $product = Product::find($id);
+
+        if(!$product){
+             return response()->json(
+                [
+                    'message' => 'No product with that ID was found'
+                ], 200
+            );
+        }
+
+        $valid = Validator::make($req->all(), [
+            'name' => 'required',
+            'description' => 'required',
+            'code' => 'unique:product',
+            'status' => 'required'
+        ]);
+
+        if ($valid->fails()) {
+            return response()->json(
+                [
+                    'message' => 'Some fields are missing or are incorrect, please check the request',
+                    'errors' => $valid->errors()
+                ], 200);
+        }
+
+        $product->name = $req->name;
+        $product->description = $req->description;
+        $product->code = $req->code;
+        $product->status = $req->status;
+
+        return response()->json([
+            'message'=>'Product updated',
+            'updatedProduct'=>$product
+        ]);
+    }
+
+    public function deleteByID($id)
+    {
+        $product = Product::find($id);
+
+        if(!$product){
+             return response()->json(
+                [
+                    'message' => 'No product with that ID was found'
+                ], 200
+            );
+        }
+
+        $product->delete();
+
+        return response()->json(['message'=> 'Product deleted','product'=> $product],200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProductController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/products', [ProductController::class,'getAll']);
+
+Route::get('/products/{id}',[ProductController::class,'getByID']);
+
+Route::put('/products/{id}',[ProductController::class,'updateByID']);
+
+Route::delete('/products/{id}',[ProductController::class,'deleteByID']);
+
+Route::post('/products/new',[ProductController::class,'save']);
